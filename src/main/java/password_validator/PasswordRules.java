7bd53f7ce17f70384771e6765ae3d225fc848edc
maksym.gorbenko <maksym.gorbenko@gmail.com>
2026-07-30 10:47:07 +0300

    Output fix

--- src/main/java/password_validator/PasswordRules.java
+++ src/main/java/password_validator/PasswordRules.java
@@ -3,6 +3,6 @@ package password_validator;
 import java.util.Set;
 
 public class PasswordRules {
-    public static final int MIN_LENGTH = 7;
+    public static final int MIN_LENGTH = 8;
     public static final Set<Character> SPECIAL_CHARACTERS = Set.of('!', '@', '#', '$', '%', '^', '&', '*');
 }
