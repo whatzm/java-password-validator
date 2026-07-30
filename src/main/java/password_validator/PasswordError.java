d0eec57de8426da13202a3312012ea6191a16c8d
maksym.gorbenko <maksym.gorbenko@gmail.com>
2026-07-30 10:35:56 +0300

    Improve output formatting

--- src/main/java/password_validator/PasswordError.java
+++ src/main/java/password_validator/PasswordError.java
@@ -1,11 +1,11 @@
 package password_validator;
 
 public enum PasswordError {
-    TOO_SHORT("Password must contain at least 8 characters."),
-    NO_DIGIT("Password must contain at least one DIGIT."),
-    NO_UPPERCASE("Password must contain at least one UPPERCASE LETTER."),
-    NO_LOWERCASE("Password must contain at least one LOWERCASE LETTER."),
-    NO_SPECIAL_CHARACTER("Password must contain at least one SPECIAL CHARACTER.");
+    TOO_SHORT("Password must contain at least " + PasswordRules.MIN_LENGTH + " characters"),
+    NO_DIGIT("Password must contain at least one DIGIT"),
+    NO_UPPERCASE("Password must contain at least one UPPERCASE LETTER"),
+    NO_LOWERCASE("Password must contain at least one LOWERCASE LETTER"),
+    NO_SPECIAL_CHARACTER("Password must contain at least one SPECIAL CHARACTER: " + PasswordRules.SPECIAL_CHARACTERS);
 
     private final String message;
 
